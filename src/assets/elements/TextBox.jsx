0e6a64cb7c66b3031529a287f701c859a93eb657
Ace <edisonford2006@gmail.com>
2025-11-11 02:50:46 +0000

    Refactor TextBox component to use <p> tags for messages; update CSS for improved styling and color scheme

--- src/assets/elements/TextBox.jsx
+++ src/assets/elements/TextBox.jsx
@@ -2,7 +2,7 @@ export default function TextBox({ messages = ["SYSTEM: Welcome to the chatbot"] 
   return (
     <div className="text-area">
       {messages.map((msg, index) => (
-        <div key={index} className="message">{msg}</div>
+        <p key={index} className="message">{msg}</p>
       ))}
     </div>
   );
