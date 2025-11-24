2fd94d2e1cd19c8aa260df0f963b3a1a6a0f06a2
Hugh <hughwooll@gmail.com>
2025-11-24 16:14:10 +0000

    Added Comments to TextBox.jsx

--- src/assets/elements/TextBox.jsx
+++ src/assets/elements/TextBox.jsx
@@ -1,6 +1,8 @@
+//Displays List of chat messages inside a scrolling text area
 export default function TextBox({ messages = ["SYSTEM: Welcome to the chatbot"] }) {
   return (
     <div className="text-area">
+      {/*Loops through messages and Renders each one as a paragraph*/}
       {messages.map((msg, index) => (
         <p key={index} className="message">{msg}</p>
       ))}
