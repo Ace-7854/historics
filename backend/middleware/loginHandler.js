0faad13787b47deb6518875c0714e21d535121e8
H.Wooll <s5713423@bournemouth.ac.uk>
2025-12-05 15:34:53 +0000

    accountLogin complete (needs fully testing)

--- backend/middleware/loginHandler.js
+++ backend/middleware/loginHandler.js
@@ -7,6 +7,38 @@ function createChatSession (username, password){
     
 }
 
-function AccountLogin (body) {
-    
+function accountLogin (body) {
+    const { username, password } = body;
+
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "db", "users.json"), "utf8"));
+
+    // Finding specific users by email 
+    const user = users.find(u => u.username === username);
+
+    if (!user) {
+        console.log("User not found");
+        return {
+            success: false,
+            message: "User not found"
+        };
+    }// PASSWORD NEEDS TO BE HASHED BEFORE COMPARISON
+    else if (user.password === password) {
+        console.log("User logged in:", username);
+        return {
+            success: true,
+            message: "Login Successful",
+            user: {
+                username: user.username,
+                chats: user.chats || []
+            }
+        };
+    }
+    else {
+        console.log("Incorrect Password");
+        return {
+            success: false,
+            message: "Incorrect Password"
+        };
+    };
+
 }
