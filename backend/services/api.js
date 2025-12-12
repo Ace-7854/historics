4576674115b9c0cdbd0f0e0936a29f5076358947
Ace <edisonford2006@gmail.com>
2025-12-12 14:34:34 +0000

    Gemini Api Model Change

--- backend/services/api.js
+++ backend/services/api.js
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 import { loadScriptPathway } from "../controllers/chatController.js";
 
-console.log("Loaded GOOGLE_API_KEY:", process.env.GOOGLE_API_KEY); // DEBUG
+// console.log("Loaded GOOGLE_API_KEY:", process.env.GOOGLE_API_KEY); // DEBUG
 
 if (!process.env.GOOGLE_API_KEY) {
   throw new Error("GOOGLE_API_KEY is missing from .env");
@@ -16,18 +16,15 @@ function gettingSysPrompt() {
   return scriptPathway.persona;
 }
 
-export async function googleApi(usrMsg) {
+export async function googleApi(usrMsg, model = "gemini-2.0-flash") {
   const systemPrompt = gettingSysPrompt();
 
-  const prompt = `
-${JSON.stringify(systemPrompt)}
-User: ${usrMsg}
-  `;
+  const prompt = `${JSON.stringify(systemPrompt)}\nUser: ${usrMsg}`;
 
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
+    model: model,
     contents: prompt,
   });
-
-  return response.text;
+  
+    return response.text;
 }
